14385d18acecb51e84a480a6b14633cec84fe182
sherzodbek18 <rsal13746@gmail.com>
2023-04-06 17:53:03 +0500

    Create print(), sinteks va arifmetik amallar.py
    Har bir misoll uchun alohida e`tibor bilan

---
+++ print(), sinteks va arifmetik amallar.py
@@ -0,0 +1,52 @@
+# Quyidagi matnni aynan shunday ko'rinishda konsolda chiqaring:
+     #      "Nexia", "Tico", 'Damas' ko'rganlar qilar havas
+
+     
+print( '"Nexia"'','' '' "Tico" ' ',' ' ' " 'Damas' " ' ' "ko`rganlar qilar havas ")     
+     
+# Quyidagi misollarga yechimni Pythonda chiqaring. Har bir misoldan avval misol matnini izoh ko'rinishida yozing:
+   
+     #       5 ning 4-darajasini toping
+a = 5 ** 4
+print(a)     
+   
+     #       22 ni 4 ga bo'lganda qancha qoldiq qoladi?
+b = 22 % 4
+print(b)     
+  
+     #       Tomonlari 125 ga teng kvadratning yuzi va perimetrini toping
+# Kvadratning tomonlari a ga teng : 
+a = 125
+
+# Ikki tomon ko`paytmasi kvadratning yuziga teng :
+b = a**2    
+    
+# Kvadratning tomonlar yig`indisi perimetrga teng :
+c = a + a + a + a    
+print( b , c )     
+   
+     #       Diametri 12 ga teng bo'lgan doiraning yuzini toping  ( π=3.14 deb oling)
+     
+p = 3.14
+
+# Radiusni aniqlab olamiz : radius = diametr / 2
+r = 12/2
+
+# Doira yuzni topish formulasi : S = π **2 
+S = p*r**2
+
+print(S)
+    
+     #    5. Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasini toping (Pifagor teoremasidan foydalaning)
+# Katet
+z = 6
+v = 7
+
+#To'g'ri burchakli uchburchakning gipotenuzasini topish formulasi : z**2 + v**2 = n**2
+
+n = z**2 + v**2
+o = n**(1/2)
+print(o)
+
+ 
+
