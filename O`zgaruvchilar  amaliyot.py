d9d12f4a74f6d110386cc8594bb5a7632ed2cdd6
sherzodbek18 <rsal13746@gmail.com>
2023-04-07 13:53:09 +0500

    Create O`zgaruvchilar  amaliyot.py
    amaliyot savollarga javoblar

---
+++ O`zgaruvchilar  amaliyot.py
@@ -0,0 +1,21 @@
+#    Quyidagi mashqlarni bajaring:
+    
+     #  "Hello World!" matnini yangi o'zgaruvchiga yuklang va print() yordamida konsolga chiqaring
+a = ' Hello World '    
+print(a)    
+     
+     #  xabar deb nomlangan o'zgaruvchiga biror matn yuklang va konsolga chiqaring, keyin esa o'zgaruvchiga yangi qiymat berib uni ham konsolga chiqaring.
+xabar = 'Python o`rganish davomida boshqa dasturlash tillarga nisabatan ancha qulay va samaradorligini tushundim !'
+print(xabar)
+
+xabar = 'Python dasturlash tili suni`y entelektda qo`llash uchun juda qulay ! '
+print(xabar)      
+     
+     #  class den nomlangan o'zgaruvchi yarating, unga biror qiymat bering va konsolga chiqaring (siz kutgan natija chiqdimi?)
+' class ' ' degan o`zgarituvchi yarata olmayman chunki bu python dastulash tilining bir  bo`lagi hisoblanadi ya`ni " class " belgilangan xususiyatlarni va metodlarni saqlaydi va ularning qiymatlarini o`zgartirish imkonini beradi'    
+     
+     #  Quyidagi kodni bajaring:
+radius = 5
+pi = 3.14159
+aylana_yuzi = pi * radius**2
+print("Radiusi" , radius, "ga teng aylananing yuzi=", aylana_yuzi)         
